c9330424333d6658fda1238eb1d161e8b3c2a22b
Miguel Chico <mchico@theworkshop.com>
2017-01-25 15:04:00 +0100

    Add test to ignore a contract

---
+++ src/test/resources/contracts/com/example/external/consumer/rest/toIgnore.groovy
@@ -0,0 +1,16 @@
+package contracts.external.rest
+
+org.springframework.cloud.contract.spec.Contract.make {
+    ignored()
+    request {
+        method 'GET'
+        url '/api/v1/xxxx'
+    }
+    response {
+        status 409
+        body([
+               status: '409',
+               message: 'CUSTOM_MESSAGE_HERE'
+        ])
+    }
+}
